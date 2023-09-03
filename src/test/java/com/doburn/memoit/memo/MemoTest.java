07e0047a80b44dc599cf23cc50c9a37bbc322675
k2852 <kwon2852@gmail.com>
2023-09-03 22:22:48 +0900

    feat: Memo 제목,내용 업데이트 메소드 테스트 추가

--- src/test/java/com/doburn/memoit/memo/MemoTest.java
+++ src/test/java/com/doburn/memoit/memo/MemoTest.java
@@ -2,6 +2,7 @@ package com.doburn.memoit.memo;
 
 import static com.doburn.memoit.common.fixtures.MemoFixtures.*;
 import static com.doburn.memoit.common.fixtures.UserFixtures.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
@@ -17,6 +18,18 @@ class MemoTest {
 		User user = 지호();
 
 		assertDoesNotThrow(() -> new Memo(메모1_제목, 메모1_내용, user));
+	}
+
+	@Test
+	@DisplayName("메모 제목과 내용을 업데이트한다.")
+	void 메모_제목과_내용을_업데이트한다() {
+		User user = 지호();
+		Memo memo = new Memo(메모1_제목, 메모1_내용, user);
+
+		memo.update(메모2_제목, 메모2_내용);
+
+		assertThat(memo.getTitle()).isEqualTo(메모2_제목);
+		assertThat(memo.getContent()).isEqualTo(메모2_내용);
 	}
 
 }
