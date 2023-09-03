06de05f5f621c50395589df8c513394f9dee3f43
k2852 <kwon2852@gmail.com>
2023-09-03 20:33:50 +0900

    메모 엔티티의 user_id FK 를 nullable=flase 로 변경

--- src/main/java/com/doburn/memoit/memo/Memo.java
+++ src/main/java/com/doburn/memoit/memo/Memo.java
@@ -29,7 +29,7 @@ public class Memo extends BaseEntity {
 	private String content;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "user_id")
+	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
 
 }
