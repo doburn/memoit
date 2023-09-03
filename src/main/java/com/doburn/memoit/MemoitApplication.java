fcd957e41988baa304d4e42d5644f91280ee82d0
k2852 <kwon2852@gmail.com>
2023-09-03 19:47:54 +0900

    스프링 프로젝트 초기화

---
+++ src/main/java/com/doburn/memoit/MemoitApplication.java
@@ -0,0 +1,13 @@
+package com.doburn.memoit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doburn/memoit/MemoitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doburn.memoit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
