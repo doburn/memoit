8a9f63399eef6593533132f94498e0c5600ef90f
k2852 <kwon2852@gmail.com>
2023-09-07 20:55:52 +0900

    feat: AuthController, AuthService 토큰 발급 메소드 추가

--- src/main/java/com/doburn/memoit/auth/controller/AuthController.java
+++ src/main/java/com/doburn/memoit/auth/controller/AuthController.java
@@ -5,8 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.doburn.memoit.auth.controller.dto.OAuthUriResponse;
-import com.doburn.memoit.global.uri.UriGenerator;
+import com.doburn.memoit.auth.dto.AuthTokenResponse;
+import com.doburn.memoit.auth.dto.OAuthUriResponse;
+import com.doburn.memoit.auth.dto.AuthTokenRequest;
+import com.doburn.memoit.auth.service.AuthService;
+import com.doburn.memoit.auth.UriGenerator;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 public class AuthController {
 
 	private final UriGenerator uriGenerator;
+	private final AuthService authService;
 
 	/**
 	 * 클라이언트에게 Google loginPage Uri 를 반환한다.
@@ -25,6 +29,23 @@ public class AuthController {
 		OAuthUriResponse oAuthUriResponse = new OAuthUriResponse(uriGenerator.generateOAuthUri());
 
 		return ResponseEntity.ok(oAuthUriResponse);
+	}
+
+	/**
+	 * 클라이언트가 유저 로그인을 확인하고, Authorization code를 전달한다.
+	 * 
+	 * Authorization code로 Google에서 token을 얻어 사용자에게 인가 정보를 넘겨준다
+	 *
+	 * 나중에 JSON으로 응답받으면 @RequestBody로 변경
+	 */
+	@GetMapping("/oauth")
+	public ResponseEntity<AuthTokenResponse> generateAccessAndRefreshToken(
+		@ModelAttribute AuthTokenRequest authTokenRequest) {
+		AuthTokenResponse authTokenResponse =
+			authService.generateAccessAndRefreshToken(uriGenerator.generateTokenUri(),
+				authTokenRequest);
+
+		return ResponseEntity.ok(authTokenResponse);
 	}
 
 }
--- src/main/java/com/doburn/memoit/auth/service/AuthService.java
+++ src/main/java/com/doburn/memoit/auth/service/AuthService.java
@@ -3,6 +3,11 @@ package com.doburn.memoit.auth.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.doburn.memoit.auth.dto.AuthTokenRequest;
+import com.doburn.memoit.auth.dto.AuthTokenResponse;
+import com.doburn.memoit.global.properties.GoogleProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -10,5 +15,13 @@ import lombok.RequiredArgsConstructor;
 @Transactional(readOnly = true)
 public class AuthService {
 
+	private final GoogleProperties googleProperties;
+	private final ObjectMapper objectMapper;
+
+	@Transactional
+	public AuthTokenResponse generateAccessAndRefreshToken(String tokenEndpointUri, AuthTokenRequest authTokenRequest) {
+
+		return new AuthTokenResponse("액세스 토큰", "리프레시 토큰");
+	}
 
 }
