7243ec35faa1122d4b4e43eafc5cb522e95b0dd2
k2852 <kwon2852@gmail.com>
2023-09-07 20:54:41 +0900

    feat: AuthService 추가

---
+++ src/main/java/com/doburn/memoit/auth/service/AuthService.java
@@ -0,0 +1,14 @@
+package com.doburn.memoit.auth.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AuthService {
+
+
+}
