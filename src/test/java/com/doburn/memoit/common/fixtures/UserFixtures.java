2f493c3e7ad6c606ba7e1b8e5f0ec4bc398776b6
k2852 <kwon2852@gmail.com>
2023-09-08 20:36:18 +0900

    refactor: 자체 플랫폼 가입 제거, Platform enum과 테스트 픽스처 수정

--- src/test/java/com/doburn/memoit/common/fixtures/UserFixtures.java
+++ src/test/java/com/doburn/memoit/common/fixtures/UserFixtures.java
@@ -12,15 +12,15 @@ public class UserFixtures {
 	/* 준형 */
 	public static final String 준형_이메일 = "joonhyung@mail.com";
 
-	/* 세욱-구글 */
+	/* 세욱 */
 	public static final String 세욱_이메일 = "sergei@gmail.com";
 
 	public static User 지호() {
-		return new User(지호_이메일, MEMOIT);
+		return new User(지호_이메일, GOOGLE);
 	}
 
 	public static User 준형() {
-		return new User(준형_이메일, MEMOIT);
+		return new User(준형_이메일, GOOGLE);
 	}
 
 	public static User 세욱() {
