77f2b6756bedd050a8c48afe7dc2d2b646455adc
k2852 <kwon2852@gmail.com>
2023-09-08 21:43:24 +0900

    feat: UserRepository 유저 상태 검증 메소드 추가

--- src/main/java/com/doburn/memoit/user/repository/UserRepository.java
+++ src/main/java/com/doburn/memoit/user/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.doburn.memoit.user.repository;
 
+import static com.doburn.memoit.user.Status.*;
+
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -20,6 +22,15 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	default void validateExistsById(Long id) {
 		if(!existsById(id)) {
 			throw new RuntimeException("존재하지 않는 회원입니다.");
+		}
+	}
+
+	default void validateStatusById(Long id) {
+		User user = findById(id)
+			.orElseThrow(() -> new RuntimeException("존재하지 않는 회원입니다."));
+
+		if(user.getStatus() != ACTIVE) {
+			throw new RuntimeException("비 활성화 회원입니다.");
 		}
 	}
 
