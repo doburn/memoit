78d8f89962ccb606b66fb74da109add1dbe0a584
k2852 <kwon2852@gmail.com>
2023-09-08 20:50:24 +0900

    feat: UserRepository 에 이메일로 회원 찾아오기 메소드 추가

--- src/main/java/com/doburn/memoit/user/repository/UserRepository.java
+++ src/main/java/com/doburn/memoit/user/repository/UserRepository.java
@@ -10,4 +10,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
 	boolean existsByEmail(String email);
 
+	Optional<User> findUserByEmail(String email);
+
+	default User getUserByEmail(String email) {
+		return findUserByEmail(email)
+			.orElseThrow(() -> new IllegalStateException("존재하지 않는 회원입니다."));
+	}
+
 }
