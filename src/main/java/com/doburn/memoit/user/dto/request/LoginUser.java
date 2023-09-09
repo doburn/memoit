38e3da1a19b33bf422d559ef1db4369d49ece14c
k2852 <kwon2852@gmail.com>
2023-09-09 23:00:05 +0900

    feat: LoginUser DTO 추가 (추후 인가 DTO로 이동)

---
+++ src/main/java/com/doburn/memoit/user/dto/request/LoginUser.java
@@ -0,0 +1,13 @@
+package com.doburn.memoit.user.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginUser {
+
+	private Long id;
+}
