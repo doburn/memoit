8fa23ef23a06778cf5c627e601aef565d91ba895
k2852 <kwon2852@gmail.com>
2023-09-10 23:05:56 +0900

    feat: AuthToken 엔티티 추가

---
+++ src/main/java/com/doburn/memoit/auth/AuthToken.java
@@ -0,0 +1,37 @@
+package com.doburn.memoit.auth;
+
+import com.doburn.memoit.global.BaseEntity;
+import com.doburn.memoit.user.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "auth_tokens")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class AuthToken extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "auth_token_id")
+	private Long id;
+
+	@Column(name = "refresh_token", nullable = false)
+	private String refreshToken;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
+}
