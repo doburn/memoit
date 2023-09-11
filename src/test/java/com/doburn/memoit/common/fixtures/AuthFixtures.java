187d62c667e8592e925878eee7d834f722e546ca
k2852 <kwon2852@gmail.com>
2023-09-11 23:23:30 +0900

    test: AuthFixtrues 토큰 픽스처 추가

---
+++ src/test/java/com/doburn/memoit/common/fixtures/AuthFixtures.java
@@ -0,0 +1,8 @@
+package com.doburn.memoit.common.fixtures;
+
+public class AuthFixtures {
+
+	public static final String ACCESS_TOKEN = "ACCESSACCESSACCESS";
+	public static final String REFRESH_TOKEN = "REFRESHREFRESHREFRESH";
+
+}
