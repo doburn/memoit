91766f038be6b0b6561589efe152b44a411c78cb
k2852 <kwon2852@gmail.com>
2023-09-12 20:58:08 +0900

    refactor: @EnableJpaAuditing 어노테이션을 별도의 JpaConfig 설정 클래스에서 처리

--- src/main/java/com/doburn/memoit/MemoitApplication.java
+++ src/main/java/com/doburn/memoit/MemoitApplication.java
@@ -2,10 +2,8 @@ package com.doburn.memoit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class MemoitApplication {
 
 	public static void main(String[] args) {
