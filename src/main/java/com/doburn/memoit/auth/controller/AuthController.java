31099730b27ace0e05d611877a2019ca69567a97
k2852 <kwon2852@gmail.com>
2023-09-13 01:02:49 +0900

    feat: AuthController 에서 로그인 URI 받는 URL을 "/oauth/login" 으로 변경

--- src/main/java/com/doburn/memoit/auth/controller/AuthController.java
+++ src/main/java/com/doburn/memoit/auth/controller/AuthController.java
@@ -23,7 +23,7 @@ public class AuthController {
 	/**
 	 * 클라이언트에게 Google loginPage Uri 를 반환한다.
 	 */
-	@GetMapping("/login")
+	@GetMapping("/oauth/login")
 	public ResponseEntity<OAuthUriResponse> generateOAuthUri() {
 
 		OAuthUriResponse oAuthUriResponse = new OAuthUriResponse(uriGenerator.generateOAuthUri());
@@ -33,7 +33,7 @@ public class AuthController {
 
 	/**
 	 * 클라이언트가 유저 로그인을 확인하고, Authorization code를 전달한다.
-	 * 
+	 *
 	 * Authorization code로 Google에서 token을 얻어 사용자에게 인가 정보를 넘겨준다
 	 *
 	 * 나중에 JSON으로 응답받으면 @RequestBody로 변경
@@ -47,5 +47,7 @@ public class AuthController {
 
 		return ResponseEntity.ok(authTokenResponse);
 	}
+
+
 
 }
